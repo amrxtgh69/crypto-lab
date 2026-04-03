360670c2a63375712b07e8d9e742850164432727
amrxtgh <ghimireamrit619@gmail.com>
2026-04-04 03:08:41 +0545

    add: main.rs

--- caesar_cipher/src/main.rs
+++ caesar_cipher/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use caesar_cipher::{encrypt, decrypt};
 
+fn main() {
+    let plaintext = "Hello World! This is a Caesar Cipher.";
+    let shift = 5;
+
+    let ciphertext = encrypt(plaintext, shift);
+    println!("Ciphertext: {}", ciphertext);
+
+    let decrypted = decrypt(&ciphertext, shift);
+    println!("Decrypted: {}", decrypted);
 }
