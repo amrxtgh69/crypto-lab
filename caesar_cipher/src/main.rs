dd77c6f1fee07cf51b69da72ac70ee816f0c96c0
amrxtgh <ghimireamrit619@gmail.com>
2026-04-05 01:13:52 +0545

    update

--- caesar_cipher/src/main.rs
+++ caesar_cipher/src/main.rs
@@ -1,7 +1,11 @@
 use caesar_cipher::{encrypt, decrypt};
 
 fn main() {
-    let plaintext = "Hello World! This is a Caesar Cipher.";
+    let plaintext = "The writer claimed by a momentary expression, \
+    a twitch of a muscle or a glance of an eye, to fathom a man’s inmost thoughts. \
+    His conclusions were as infallible as so many propositions of Euclid. \
+    So startling would his results appear to the uninitiated that until they learned the \
+    processes by which he had arrived at them they might well consider him as a necromancer";
     let shift = 5;
 
     let ciphertext = encrypt(plaintext, shift);
