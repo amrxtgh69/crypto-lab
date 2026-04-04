a2eef5f26b78f2f83149e792c4bef1831b8dadc2
amrxtgh <ghimireamrit619@gmail.com>
2026-04-05 01:36:33 +0545

    update

--- caesar_cipher/src/frequency_analysis.rs
+++ caesar_cipher/src/frequency_analysis.rs
@@ -22,8 +22,29 @@
  * here i am implementing just caesar cipher attack brute force: 
  */
 
+
+//  `ciphertext
+//    ↓
+// try shift k
+//    ↓
+// decrypted text
+//    ↓
+// count letters
+//    ↓
+// compare with English frequency`
+
 use crate::decrypt;
+use core::f64;
 use std::usize;
+
+// The frequency of letters in English text.
+const ENGLISH_FREQ: [f64; 26] = [
+    8.15, 1.44, 2.76, 3.79, 13.11, 2.92, 1.99,
+    5.26, 6.35, 0.13, 0.42, 3.39, 2.54,
+    7.10, 8.00, 1.98, 0.12, 6.83,
+    6.10, 10.47, 2.46, 0.92, 1.54,
+    0.17, 1.98, 0.08
+];
 
 pub fn break_caesar(ciphertext: &str) -> (u8, String) {
     let shift = find_shift(ciphertext);
@@ -33,16 +54,33 @@ pub fn break_caesar(ciphertext: &str) -> (u8, String) {
 
 
 fn find_shift(ciphertext: &str) -> u8 {
-    let counts = count_letters(ciphertext);
-    let most_freq = most_frequent_letter(&counts);
-    
+    let mut best_shift = 0;
+    let mut best_score = f64::INFINITY;
+
+    for shift in 0..26 {
+        let decrypted = decrypt(ciphertext, shift);
+
+        let counts = count_letters(&decrypted);
+        let freq = normalize(&counts);
+
+        let s = score(&freq);
+
+        if s < best_score {
+            best_score = s;
+            best_shift = shift;
+        }
+    }
+    best_shift
 }
 
+fn normalize(counts: &[u32; 26]) -> [f64; 26] {
+    todo!();
+}
+fn score(freq: &[f64; 26]) -> f64 {
+    todo!();
+}
 fn count_letters(text: &str) -> [u32; 26] {
     let mut counts = [0; 26];
-~/crypto-lab/caesar_cipher git:(main) ❯ tree
-.
-
     for c in text.chars() {
         if c.is_ascii_alphabetic() {
             let idx = (c.to_ascii_uppercase() as u8 - b'A') as usize;
@@ -52,4 +90,7 @@ fn count_letters(text: &str) -> [u32; 26] {
     counts
 }
 
+fn most_frequent_letter(counts: &str) -> u8 {
+    todo!()
+}
 
